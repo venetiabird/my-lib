815c7dd48960e42d67dec669af7d7ff83cc7540d
venetia-bird <venetia.bird@.realestate.com.au>
2020-10-14 15:14:02 +1100

    basic api extractor setup

--- src/index.ts
+++ src/index.ts
@@ -41,3 +41,5 @@ export function sum2(a: number, b: number): number {
   const sum = a + b;
   return sum;
 }
+
+export { subtract } from "./subtract"; 
---
+++ src/subtract.ts
@@ -0,0 +1,12 @@
+/**
+ * Calculate the difference between two numbers
+ * 
+ * @param a - first number
+ * @param b - second number
+ * 
+ * @beta
+ */
+
+export function subtract(a: number, b: number): number {
+  return a - b
+}
